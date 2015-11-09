731d4ea476fde66f74ae09cd9f732a48f6277844
Diego Bezerra <diego@tatulab.com>
2015-11-09 14:55:00 -0300

    BubbleSort.

--- ordenacao/C/bubbleSort_static_worst.c
+++ ordenacao/C/bubbleSort_static_worst.c
@@ -1,5 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-	return 0;
+int main()
+{
+    int V[] = {3, -2, 0, -1, 6};
+    int n = 5;
+    int i, j, temp;
+
+    printf("Original: ");
+    for (i = 0; i < n; i++)
+        printf("%3d", V[i]);
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n - 1; j++)
+            if (V[j] > V[j + 1]) {
+                temp = V[j];
+                V[j] = V[j + 1];
+                V[j + 1] = temp;
+            }
+
+    printf("\nOrdenado: ");
+    for (i = 0; i < n; i++)
+        printf("%3d", V[i]);
+
+    return 0;
 }
